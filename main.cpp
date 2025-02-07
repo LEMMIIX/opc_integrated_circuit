6fafb5932aa45abb156cab5dd50c36d774634036
LEMMIIX <lemmiix@gmail.com>
2025-02-07 18:12:28 +0100

    Improve the try/catch block in the main function.
    Quality of life improvements: the sensor names are now included in the programm arguments. Currently there is no error handling if no arguments are provided.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <stdexcept>
 #include <iostream>
 #include <ctime>
+#include <utility>
 
 #include "opcpp_ic.h"
 
@@ -9,19 +10,43 @@ const bool& system_is_powered_up{true};
 
 int main(int argc, char const *argv[])
 {
+    // TASK:
+    // upon program start, configure and set up sensor names,
+    // TODO: rerun failed sensor starts
+    // via cli
+
+    // ./main.exe name1 name2 name3 name4
+
+    const int sensor_count{4};
+
+    std::string sensor_names[sensor_count];
+    for (int i = 0; i < sensor_count; ++i) {
+        sensor_names[i] = argv[i + 1];
+    }
+
     try {
-        std::cout << "Attempting to power up the sensor..." << std::endl;
-        power_up_sensor(Sensor::radar, "pipi");
-        std::cout << "Successfully powered up the sensor" << std::endl;
+        std::cout << "Attempting to power up the sensors, named:" << std::endl;
+        for (int i = 0; i < sensor_count; ++i) {
+            std::cout << sensor_names[i] << std::endl;
+        }
+        
+        power_up_sensor(Sensor::lidar,      sensor_names[0]);
+        power_up_sensor(Sensor::radar,      sensor_names[1]);
+        power_up_sensor(Sensor::ultrasonic, sensor_names[2]);
+        power_up_sensor(Sensor::pressure,   sensor_names[3]);
+        
+        std::cout << "Successfully powered up the sensors" << std::endl;
 
     } catch (const std::runtime_error& e) {
-        std::cout << "Powering up the sensor did not work and caught runtime exception: "
+        std::cout << "Powering up the sensors did not work and caught runtime exception: "
         << e.what() << std::endl;
 
     } catch (const std::exception& e) {
-        std::cout << "Powering up the sensor did not work and caught exception: "
+        std::cout << "Powering up the sensors did not work and caught exception: "
         << e.what() << std::endl;
     }
+
+    
 
     return 0;
 }
