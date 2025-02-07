2f8427de34ecaef7a70fc04f1d1759e7d8c2aba7
LEMMIIX <lemmiix@gmail.com>
2025-02-07 18:51:49 +0100

    Add simple calibration reads of all sensors.

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@ int main(int argc, char const *argv[])
         sensor_names[i] = argv[i + 1];
     }
 
+    // Power up sensor
     try {
         std::cout << "Attempting to power up the sensors, named:" << std::endl;
         for (int i = 0; i < sensor_count; ++i) {
@@ -40,13 +41,41 @@ int main(int argc, char const *argv[])
     } catch (const std::runtime_error& e) {
         std::cout << "Powering up the sensors did not work and caught runtime exception: "
         << e.what() << std::endl;
-
+        
     } catch (const std::exception& e) {
         std::cout << "Powering up the sensors did not work and caught exception: "
         << e.what() << std::endl;
     }
-
     
+    // Read calibrations
+    // es kann immer nur 1 mal pro Sensor die Konfiguration abgefragt werden
+    try {
+        
+        // auto calib_1 = std::get<0>(get_calibration(Sensor::lidar,      sensor_names[0]));
+        // auto calib_2 = std::get<0>(get_calibration(Sensor::radar,      sensor_names[1]));
+        // auto calib_3 = std::get<0>(get_calibration(Sensor::ultrasonic, sensor_names[2]));
+        // auto calib_4 = std::get<0>(get_calibration(Sensor::pressure,   sensor_names[3]));
+        
+        auto val_1 = std::get<1>(get_calibration(Sensor::lidar,      sensor_names[0]));
+        auto val_2 = std::get<1>(get_calibration(Sensor::radar,      sensor_names[1]));
+        auto val_3 = std::get<1>(get_calibration(Sensor::ultrasonic, sensor_names[2]));
+        auto val_4 = std::get<1>(get_calibration(Sensor::pressure,   sensor_names[3]));
+        
+        std::cout << "Calibrations:\n"
+            << /*calib_1 <<*/ "\t" << val_1 << "\n" 
+            << /*calib_2 <<*/ "\t" << val_2 << "\n"
+            << /*calib_3 <<*/ "\t" << val_3 << "\n"
+            << /*calib_4 <<*/ "\t" << val_4 << "\n"
+            << std::endl;
+    
+    } catch (const std::runtime_error& e) {
+        std::cout << "Reading calibration did not work and caught runtime exception: "
+        << e.what() << std::endl;
 
+    } catch (const std::exception& e) {
+        std::cout << "Reading calibration did not work and caught exception: "
+        << e.what() << std::endl;
+    }
+    
     return 0;
 }
