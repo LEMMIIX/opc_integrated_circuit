4b185f9f4181c1b75ad2783177727cb9717664c3
LEMMIIX <lemmiix@gmail.com>
2025-02-16 20:00:29 +0100

    Add slightly better terminal formatting and small variable refactoring

--- main.cpp
+++ main.cpp
@@ -18,10 +18,11 @@ const bool& system_is_powered_up{true};
 int main(int argc, char const *argv[])
 {
     // TASK:
-    // TODO: rerun failed sensor starts
     // via cli
 
     // ./main.exe name1 name2 name3 name4
+
+    std::cout << "Launching" << std::endl;
 
     Sensor sensor_types[]{
         Sensor::lidar,
@@ -30,7 +31,7 @@ int main(int argc, char const *argv[])
         Sensor::pressure,
     };
 
-    const int sensor_count{sizeof(sensor_types)};
+    const int sensor_count{sizeof(sensor_types) / sizeof(sensor_types[0])};
 
     std::string sensor_names[sensor_count];
     for (int i = 0; i < sensor_count; ++i) {
@@ -43,7 +44,7 @@ int main(int argc, char const *argv[])
     }
 
     // Power up sensor
-    std::cout << "Attempting to power up the sensors..." << std::endl;
+    std::cout << "\nAttempting to power up the sensors..." << std::endl;
     try {
         bool is_powered_up{false};
         for(int i = 0; i < sensor_count; ++i) {
@@ -52,7 +53,7 @@ int main(int argc, char const *argv[])
                 is_powered_up = (power_up_sensor(sensor_types[i], sensor_names[i]) == Status::success);
             }
             
-            std::cout << "Powered up sensor:\t" << sensor_names[i];
+            std::cout << "Powered up sensor:\t" << sensor_names[i] << std::endl;
         }
 
 
@@ -72,7 +73,7 @@ int main(int argc, char const *argv[])
         << e.what() << std::endl;
     }
     
-    std::cout << "Attempting to apply sensor calibrations..." << std::endl;
+    std::cout << "\nAttempting to apply sensor calibrations..." << std::endl;
     try {
         apply_sensor_calibration(Sensor::lidar,         sensor_names[0], "123");
         apply_sensor_calibration(Sensor::radar,         sensor_names[1], "123");
@@ -90,23 +91,23 @@ int main(int argc, char const *argv[])
 
     // Read calibrations
     // es kann immer nur 1 mal pro Sensor die Konfiguration abgefragt werden
-    std::cout << "Fetching calibrations:" << std::endl;
+    std::cout << "\nFetching calibrations:" << std::endl;
     try {
         
-        // auto calib_1 = std::get<0>(get_calibration(Sensor::lidar,      sensor_names[0]));
-        // auto calib_2 = std::get<0>(get_calibration(Sensor::radar,      sensor_names[1]));
-        // auto calib_3 = std::get<0>(get_calibration(Sensor::ultrasonic, sensor_names[2]));
-        // auto calib_4 = std::get<0>(get_calibration(Sensor::pressure,   sensor_names[3]));
+        auto calib_1 = std::get<0>(get_calibration(Sensor::lidar,      sensor_names[0]));
+        auto calib_2 = std::get<0>(get_calibration(Sensor::radar,      sensor_names[1]));
+        auto calib_3 = std::get<0>(get_calibration(Sensor::ultrasonic, sensor_names[2]));
+        auto calib_4 = std::get<0>(get_calibration(Sensor::pressure,   sensor_names[3]));
         
-        auto val_1 = std::get<1>(get_calibration(Sensor::lidar,      sensor_names[0]));
-        auto val_2 = std::get<1>(get_calibration(Sensor::radar,      sensor_names[1]));
-        auto val_3 = std::get<1>(get_calibration(Sensor::ultrasonic, sensor_names[2]));
-        auto val_4 = std::get<1>(get_calibration(Sensor::pressure,   sensor_names[3]));
+        // auto val_1 = std::get<1>(get_calibration(Sensor::lidar,      sensor_names[0]));
+        // auto val_2 = std::get<1>(get_calibration(Sensor::radar,      sensor_names[1]));
+        // auto val_3 = std::get<1>(get_calibration(Sensor::ultrasonic, sensor_names[2]));
+        // auto val_4 = std::get<1>(get_calibration(Sensor::pressure,   sensor_names[3]));
         
-        std::cout << /*calib_1 <<*/ "\t" << val_1 << "\n" 
-        /*<< calib_2 << "\t"*/ << val_2 << "\n"
-        /*<< calib_3 << "\t"*/ << val_3 << "\n"
-        /*<< calib_4 << "\t"*/ << val_4 << "\n"
+        std::cout   << calib_1 << "\t"  << /*val_1 << */"\n" 
+                    << calib_2 << "\t"  << /*val_2 << */"\n"
+                    << calib_3 << "\t"  << /*val_3 << */"\n"
+                    << calib_4 << "\t"  /*<< val_4 << "\n"*/
         << std::endl;
         
     } catch (const std::runtime_error& e) {
@@ -118,7 +119,7 @@ int main(int argc, char const *argv[])
         << e.what() << std::endl;
     }
     
-    std::cout << "Reading register data" << std::endl;
+    std::cout << "\nReading register data" << std::endl;
     try {
         std::cout << read_reg(0x1) << std::endl
         << read_reg(0x2) << std::endl
@@ -133,7 +134,7 @@ int main(int argc, char const *argv[])
         << e.what() << std::endl;
     }
     
-    std::cout << "Writing to register(s)..." << std::endl;
+    std::cout << "\nWriting to register(s)..." << std::endl;
     try {
         const unsigned short data{11111};
         write_reg(0x3, data);
@@ -148,19 +149,16 @@ int main(int argc, char const *argv[])
         << e.what() << std::endl;
     } 
     
-    std::cout << "Attempting to enter test mode..." << std::endl;
+    std::cout << "\nAttempting to enter test mode..." << std::endl;
     bool was_success{false};
     try{
         int attempts{5};
-        Status status;
         
-        
-        for (int i = 0; i < attempts && !was_success; ++i) {
-            status = enter_test_mode();
+        while(!was_success) {
             
-            was_success = status == Status::success;
+            was_success = enter_test_mode() == Status::success;
             
-            std::cout << "Status, attempt[" << i << "]: " << (was_success ? "success" : "failed") << std::endl;
+            std::cout << "\tStatus: " << (was_success ? "success" : "failed") << std::endl;
             
         }
     } catch (const std::runtime_error& e) {
@@ -173,18 +171,16 @@ int main(int argc, char const *argv[])
     }  
     
     // entering jtag mode if previous test mode entering was successful
-    std::cout << "Attempting to jtag mode..." << std::endl;
+    std::cout << "\nAttempting to jtag mode..." << std::endl;
     if (was_success) {
         try {
-            int attempts{5};
-            Status status;
+            was_success = false;
             
-            for (int i = 0; i < attempts && !was_success; ++i) {
-                status = enter_jtag();
+            while(!was_success) {
                 
-                was_success = status == Status::success;
+                was_success = enter_jtag() == Status::success;
                 
-                std::cout << "Status, attempt[" << i << "]: " << (was_success ? "success" : "failed") << std::endl;
+                std::cout << "\tStatus:\t" << (was_success ? "success" : "failed") << std::endl;
                 
             }
         } catch (const std::runtime_error& e) {
@@ -200,24 +196,22 @@ int main(int argc, char const *argv[])
     }
     
     // running scan tests if test mode entering was successful
-    std::cout << "Attempting to run scan tests..." << std::endl;
+    std::cout << "\nAttempting to run scan tests..." << std::endl;
     if (was_success) {
-        int attempts{5};
-        Status status;
+        was_success = false;
         
-        for (int i = 0; i < attempts && !was_success; ++i) {
-            status = run_scan_test();
+        while(!was_success) {
             
-            was_success = status == Status::success;
+            was_success = run_scan_test() == Status::success;
             
-            std::cout << "Status, attempt[" << i << "]: " << (was_success ? "success" : "failed") << std::endl;
+            std::cout << "\tStatus: " << (was_success ? "success" : "failed") << std::endl;
             
         }
     } else {
         std::cout << "Previous test mode entering was unsuccessfull - SKIPPING to run tests." << std::endl;
     }
     
-    std::cout << "Attempting to exit jtag mode..." << std::endl;
+    std::cout << "\nAttempting to exit jtag mode..." << std::endl;
     try {
         exit_jtag();
     } catch (const std::runtime_error& e) {
@@ -228,7 +222,7 @@ int main(int argc, char const *argv[])
         << e.what() << std::endl;
     }
     
-    std::cout << "Attempting to exit test mode..." << std::endl;
+    std::cout << "\nAttempting to exit test mode..." << std::endl;
     try {
         exit_test_mode();
     } catch (const std::runtime_error& e) {
@@ -239,7 +233,7 @@ int main(int argc, char const *argv[])
         << e.what() << std::endl;
     }
     
-    std::cout << "Attempting to power down sensors..." << std::endl;
+    std::cout << "\nAttempting to power down sensors..." << std::endl;
     try {
         power_down_sensor(Sensor::lidar,      sensor_names[0]);
         power_down_sensor(Sensor::radar,      sensor_names[1]);
